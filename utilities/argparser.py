7b68e811f7f58e941a1b32f3e77256edca760122
blake <blake@secureset.com>
2019-08-25 23:05:51 -0600

    updated input parameters

--- utilities/argparser.py
+++ utilities/argparser.py
@@ -3,7 +3,7 @@ import argparse
 #defined functions 
 def input_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', action='store', dest='debug',default=False,
+    parser.add_argument('-d', action='store_true', dest='debug',default=False,
             help='Store the value for debug, default set to false.')
     parser.add_argument("-f",action='store', dest='filename',default="list_of_ips.txt",
             help='Store the input name of text file, default set to list_of_ips.txt')
